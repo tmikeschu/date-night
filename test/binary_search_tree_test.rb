52850c3b44e9bb1300cdf63e6988782bec74dbbc
Mike Schutte <tmikeschutte@gmail.com>
2016-10-12 22:15:30 -0600

    change file names to match class convention

--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -1,7 +1,7 @@
 gem 'minitest', '~> 5.2'
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/date_night'
+require './lib/binary_search_tree'
 require 'pry'
 
 class BinarySearchTreeTest < Minitest::Test
